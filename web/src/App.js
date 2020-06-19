a67cf6ed320db9b34b8e2ef698aaf2421461b76a
BrenoLuna0 <breno.luna_@hotmail.com>
2020-06-19 17:41:09 -0300

    esboço inicial do grid layout

--- web/src/App.js
+++ web/src/App.js
@@ -1,25 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import "./global.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <header>
+      <div className="header">
+        <div className="headerContent">
+          <div className="logo">logo</div>
+          <div className="produtos">Produtos</div>
+          <div className="contato">Contato</div>
+          <div className="carrinho">Carrinho</div>
+          <div className="logout">Logout</div>
+        </div>
+      </div>
+    </header>
   );
 }
 
