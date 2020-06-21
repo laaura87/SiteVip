f155054053ea505e9ad49538cb2f2beea0142eae
BrenoLuna0 <breno.luna_@hotmail.com>
2020-06-21 12:53:07 -0300

    Animações no header para os produtos e o carrinho prontas

--- web/src/components/Header/index.js
+++ web/src/components/Header/index.js
@@ -1,52 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./style.css";
 
-/**
- * <img
-              className="logo"
-              src={`${process.env.PUBLIC_URL}/images/vip_logo.png`}
-              alt="Logo"
-            />
- */
-
 function Component() {
+  const [categoryHidden, setCategoryHidden] = useState("");
+  const [disabled, setDisabled] = useState("");
+  const [cart, setCart] = useState("");
+
+  const handleProdutoHoverUp = () => {
+    setCategoryHidden("animation");
+  };
+
+  const handleProdutoHoverDown = () => {
+    setCategoryHidden("");
+  };
+
+  const handleCartClick = () => {
+    setDisabled("disabled");
+    setCart("cart");
+  };
+
+  const disableCart = () => {
+    setDisabled("");
+    setCart("");
+  };
+
   return (
-    <header>
-      <div className="header">
-        <div className="headerContent clickable">
-          <div className="logo">
-            <img
-              className="logo"
-              src={`${process.env.PUBLIC_URL}/images/vip_logo.png`}
-              alt="Logo"
-              width="120"
-              height="120"
-            />
+    <>
+      <header>
+        <div className="header">
+          <div className="headerContent clickable">
+            <div className="logo">
+              <img
+                className="logo"
+                src={`${process.env.PUBLIC_URL}/images/vip_logo.png`}
+                alt="Logo"
+                width="120"
+                height="120"
+              />
+            </div>
+            <div
+              className={`produtos clickable`}
+              onMouseOver={() => handleProdutoHoverUp()}
+              onMouseOut={() => handleProdutoHoverDown()}
+            >
+              <p>Produtos</p>
+            </div>
+            <div className="contato clickable">Contato</div>
+            <div
+              className="carrinho clickable"
+              onClick={() => handleCartClick()}
+            >
+              Carrinho
+            </div>
+            <div className="logout clickable">Logout</div>
           </div>
-          <div className="produtos clickable">Produtos</div>
-          <div className="contato clickable">Contato</div>
-          <div className="carrinho clickable">Carrinho</div>
-          <div className="logout clickable">Logout</div>
         </div>
-      </div>
-      <div className="container">
-        <div className="headerContent">
-          <div className="test">1</div>
-          <div className="test">2</div>
-          <div className="test">3</div>
-          <div className="test">4</div>
-          <div className="test">5</div>
-          <div className="test">6</div>
-          <div className="test">7</div>
-          <div className="test">8</div>
-          <div className="test">9</div>
-          <div className="test">10</div>
-          <div className="test">11</div>
-          <div className="test">12</div>
+        <div
+          className={`container ${categoryHidden}`}
+          onMouseOver={() => handleProdutoHoverUp()}
+          onMouseOut={() => handleProdutoHoverDown()}
+        >
+          <div className="headerContent"></div>
         </div>
-      </div>
-    </header>
+      </header>
+      <div className={disabled}></div>
+      <div className={cart} onClick={() => disableCart()}></div>
+    </>
   );
 }
 
